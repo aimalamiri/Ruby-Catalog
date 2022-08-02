55a565eec00b3ad6c50ffaf5462cfff3af17756d
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-02 16:31:53 +0200

    Add  publish date from parent by inheritance

--- classes/game.rb
+++ classes/game.rb
@@ -1,7 +1,10 @@
-class Game
+require_relative './item'
+
+class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
   def initialize(multiplayer, last_played_at)
+    super(publish_date)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
