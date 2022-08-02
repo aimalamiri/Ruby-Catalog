e25e62321d42c245422cc12c2ed49a0b180ce5f9
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-02 10:44:10 +0200

    Add logic to get user inputs

--- classes/app.rb
+++ classes/app.rb
@@ -1,12 +1,15 @@
 class App
   def list_of_options
     { 0 => 'Genere section', 1 => 'Source section', 2 => 'Authors', 3 => 'Labels', 4 => 'Books section',
-      5 => 'Music Albums', 6 => 'Movies', 7 => 'Exit' }
+      5 => 'Music Albums', 6 => 'Games', 7 => 'Exit' }
   end
 
   def run
     list_of_options.each do |key, value|
       puts "#{key} - #{value}"
     end
+    puts 'Choose an option by number: '
+    input = gets.chomp.to_i
+    exit if input == 7
   end
 end
