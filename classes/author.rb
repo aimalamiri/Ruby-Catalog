4a6701089bbd0bfe147f3bc012dc909c3e24721d
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-02 15:25:17 +0200

    Add author class and logic

---
+++ classes/author.rb
@@ -0,0 +1,14 @@
+require_relative './app'
+require 'securerandom'
+
+class Author
+  attr_accessor :firstname, :lastname
+  attr_reader :id, :items
+
+  def initialize(firstname, lastname)
+    @id = SecureRandom.uuid
+    @firstname = firstname
+    @lastname = lastname
+    @items = []
+  end
+end
