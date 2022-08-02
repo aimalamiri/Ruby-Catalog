b2a6f11f4647b1f8625f66fd2b029779ddb8793d
Aimal Amiri <meaimal@hotmail.com>
2022-08-02 12:36:23 +0430

    Add the list of options in the app.run method

--- classes/app.rb
+++ classes/app.rb
@@ -1,5 +1,12 @@
 class App
+  def list_of_options
+    { 0 => 'Genere section', 1 => 'Source section', 2 => 'Authors', 3 => 'Labels', 4 => 'Books section',
+      5 => 'Music Albums', 6 => 'Movies', 7 => 'Exit' }
+  end
+
   def run
-    # Return the basic concept
+    list_of_options.each do |key, value|
+      puts "#{key} - #{value}"
+    end
   end
 end
