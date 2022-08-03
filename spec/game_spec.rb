a2a9b97be13cd3fcbf5c753c2f961b941f2584be
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-03 14:06:42 +0200

    Add tests for can be archieved methods

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,7 +3,7 @@ require_relative 'spec_helper'
 describe Game do
   context 'Testing Games' do
     before(:each) do
-      @game = Game.new('2012-06-12', 'Y', '2011-05-12')
+      @game = Game.new('2011-06-12', 'Y', '2014-05-12')
     end
 
     it 'Should be an instance of the Game class' do
@@ -15,7 +15,11 @@ describe Game do
     end
 
     it 'Should add one game to the collection' do
-      expect(@game.last_played_at).to eq '2011-05-12'
+      expect(@game.last_played_at).to eq '2014-05-12'
+    end
+
+    it 'Should return true as the method can be archived is true' do
+      expect(@game.can_be_archived?).to be true
     end
   end
 end
