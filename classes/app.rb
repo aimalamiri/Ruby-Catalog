9720bd866cdacc711541cf894108e3a914d41ed3
Aimal Amiri <meaimal@hotmail.com>
2022-08-03 15:10:30 +0430

    Arrange the actions list in app class

--- classes/app.rb
+++ classes/app.rb
@@ -1,15 +1,18 @@
 class App
-  def list_of_options
-    { 0 => 'Genere section', 1 => 'Source section', 2 => 'Authors', 3 => 'Labels', 4 => 'Books section',
-      5 => 'Music Albums', 6 => 'Games', 7 => 'Exit' }
+  def run
+    list_of_options.each { |key, value| puts "#{key} - #{value}" }
+    puts 'Choose an option by number: '
+    action = gets.chomp.to_i
+    action(action)
   end
 
-  def run
-    list_of_options.each do |key, value|
-      puts "#{key} - #{value}"
-    end
-    puts 'Choose an option by number: '
-    input = gets.chomp.to_i
-    exit if input == 7
+  def list_of_options
+    { 1 => 'List all items', 2 => 'List all authors', 3 => 'List all geners', 4 => 'List all labels',
+      5 => 'List all music albums', 6 => 'Add new item', 7 => 'Add new author', 8 => 'Add new gener',
+      9 => 'Add new label', 10 => 'Add new music album', 0 => 'Exit' }
+  end
+
+  def action(action)
+    puts list_of_options[action]
   end
 end
