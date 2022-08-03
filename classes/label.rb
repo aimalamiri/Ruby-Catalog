8b7dc1edebeefd725397fbc2febc206b371237aa
Aimal Amiri <meaimal@hotmail.com>
2022-08-03 13:17:31 +0430

    Fix bug in the label class

--- classes/label.rb
+++ classes/label.rb
@@ -2,6 +2,7 @@ require 'securerandom'
 
 class Label
   attr_accessor :title, :color
+  attr_reader :items
 
   def initialize(title, color)
     @id = SecureRandom.uuid
@@ -11,7 +12,7 @@ class Label
   end
 
   def add_item(item)
-    item.label = item
+    item.label = self
     @items << item
   end
 end
