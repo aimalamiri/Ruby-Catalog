9ba25c5b9fd60201ad74323ed530bf83739cf338
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-04 18:05:08 +0200

    Add schema diagram SQL queries

--- schema.sql
+++ schema.sql
@@ -20,3 +20,26 @@ CREATE TABLE books (
   CONSTRAINT fk_source_id FORIEGN KEY (source_id) REFERENCES sources(id),
   CONSTRAINT fk_label_id FORIEGN KEY (label_id) REFERENCES labels(id)
 );
+
+CREATE TABLE authors (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR,
+  last_name VARCHAR
+);
+
+
+CREATE TABLE games (
+  id SERIAL PRIMARY KEY,
+  genre_id INT,
+  author_id INT,
+  source_id INT,
+  label_id INT,
+  publish_date DATE,
+  archived BOOLEAN,
+  multiplayer BOOLEAN,
+  last_played_at DATETIME,
+  CONSTRAINT fk_genre_id FORIEGN KEY (genre_id) REFERENCES genres(id),
+  CONSTRAINT fk_author_id FORIEGN KEY (author_id) REFERENCES authors(id),
+  CONSTRAINT fk_source_id FORIEGN KEY (source_id) REFERENCES sources(id),
+  CONSTRAINT fk_label_id FORIEGN KEY (label_id) REFERENCES labels(id)
+);
