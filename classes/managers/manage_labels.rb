323dca5a664988e6c618116d9b2d2d9cfd364ef8
Aimal Amiri <meaimal@hotmail.com>
2022-08-04 15:58:04 +0430

    The user can read the labels from file

--- classes/managers/manage_labels.rb
+++ classes/managers/manage_labels.rb
@@ -1,13 +1,16 @@
 require_relative '../label'
 require_relative '../io/file_writer'
+require_relative '../io/file_reader'
 
 class ManageLabels
   def initialize(labels)
     @labels = labels
     @file_writer = FileWriter.new('labels.json')
+    @file_reader = FileReader.new('labels.json')
   end
 
   def list_labels
+    load_labels
     @labels.each do |label|
       puts "Label - #{label.title} is #{label.color}"
     end
@@ -25,5 +28,9 @@ class ManageLabels
     @file_writer.write_data(label)
 
     puts "The label #{label.title} has been added successfully!"
+  end
+
+  def load_labels
+    @labels = @file_reader.read_data
   end
 end
